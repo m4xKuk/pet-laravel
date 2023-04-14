ded6baceb8b15524fd7b98914a8e4e6cae1dde0c
maks <rammerpp@gmail.com>
2023-04-14 09:35:12 +0300

    models Post & Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $table = 'categories';
+    protected $guarded = false;
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'category_posts', 'category_id', 'post_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $table = 'posts';
+    protected $guarded = false;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_posts', 'post_id', 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
