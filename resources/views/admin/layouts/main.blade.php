9da48b6ccba4528fd5596e00365420a84293d8bf
maks <rammerpp@gmail.com>
2023-04-14 22:46:29 +0300

    add builder vite

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Dashboard</title>
-
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
